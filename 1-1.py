2dd3da58dd045ff1ed1b0e9a1061ed54e471638f
LawrenceChou19 <lawrencechou19@gmail.com>
2024-03-13 19:38:10 +0800

    2024-03-13

--- 1-1.py
+++ 1-1.py
@@ -8,3 +8,17 @@ for i in range(1,n+1):
     sum = sum + i
     print('Result : ',sum)
     separate_section()
+    
+print('Single for loop design calculator series')
+sum = 0
+termi = 1 
+x = 1.4
+for i in range(0,6):
+    print('Sum    is :',sum)
+    print('termi  is :',termi)
+    sum += termi
+    print('Sum+   is :',sum)
+    termi = termi * x/2
+    print('termi* is :',termi)
+    separate_section()
+    
